6bfba1e94cc05c64335896f270558b6a69a94467
giacomo <rota.giacomo.studente@itispaleocapa.it>
2025-11-12 13:25:42 +0100

    Metodi della classe Coordinate

--- Coordinate.java
+++ Coordinate.java
@@ -1,4 +1,61 @@
 public class Coordinate {
     private double latitudine;
     private double longitudine;
+
+    public void setLatitudine(double latitudine){
+        if (latitudine >= -90 && latitudine <= 90){
+            this.latitudine = latitudine;
+        }else {
+            this.latitudine = 200;
+        }
+    }
+
+    public void setLongitudine(double longitudine) {
+        if (longitudine >= -180 && longitudine <= 180){
+            this.longitudine = longitudine;
+        }else {
+            this.longitudine = 200;
+        }
+    }
+
+    public double getLatitudine() {
+        return latitudine;
+    }
+
+    public double getLongitudine() {
+        return longitudine;
+    }
+
+    public Coordinate(){
+        latitudine = 0;
+        longitudine = 0;
+    }
+
+    public Coordinate(double latitudine, double longitudine){
+        setLatitudine(latitudine);
+        setLongitudine(longitudine);
+    }
+
+    public boolean equals(Coordinate cordinateDaConfrontare){
+
+        return latitudine == cordinateDaConfrontare.latitudine && longitudine == cordinateDaConfrontare.longitudine;
+    }
+
+    public boolean isInArea(Coordinate minime, Coordinate massime){
+        if (minime.latitudine > massime.latitudine){
+            double temp = minime.latitudine;
+            minime.latitudine = massime.latitudine;
+            massime.latitudine = temp;
+        }
+        if (minime.longitudine > massime.longitudine){
+            double temp = minime.longitudine;
+            minime.longitudine = massime.longitudine;
+            massime.longitudine = temp;
+        }
+        return latitudine >= minime.latitudine && latitudine <= massime.latitudine && longitudine >= minime.longitudine && longitudine <= massime.longitudine;
+    }
+
+    public String toString(){
+        return "Coordinate[" + latitudine + ", " + longitudine + "]";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
-
+        Coordinate minime = new Coordinate(30, -20);
+        Coordinate massime = new Coordinate(15,  50);
+        Coordinate primoPunto = new Coordinate(20,0);
+        Coordinate secondoPunto = new Coordinate(-10, 10);
+        System.out.println(primoPunto.isInArea(minime, massime));
+        System.out.println(secondoPunto.isInArea(minime, massime));
     }
 }
